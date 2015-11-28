8d7502d66b14ea96d7090e780564b0b2288c1d8c
Steinar Valsson <steinarv12@ru.is>
2015-11-28 22:11:24 +0000

    Script for updating the docker image on the test machine

---
+++ buildOnTest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "SSH to Test machine..."
+ssh 192.168.56.77 '
+docker stop tic
+docker rm tic
+docker pull steinarv12/tictactoe:latest
+docker run -p 9000:8080 -d --name="tic" -e "NODE_ENV=production" steinarv12/tictactoe
+
+'
+echo "Commands sent"
