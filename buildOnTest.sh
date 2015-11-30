0f7191896603d376c9c1eb36b5ada532d3fb5125
Steinar Valsson <steinarv12@ru.is>
2015-11-30 11:27:48 +0000

    NODE_ENV changed from production to test

--- buildOnTest.sh
+++ buildOnTest.sh
@@ -5,7 +5,7 @@ ssh 192.168.56.77 '
 docker stop tic
 docker rm tic
 docker pull steinarv12/tictactoe:latest
-docker run -p 9000:8080 -d --name="tic" -e "NODE_ENV=production" steinarv12/tictactoe
+docker run -p 9000:8080 -d --name="tic" -e "NODE_ENV=test" steinarv12/tictactoe
 
 '
 echo "Commands sent"
