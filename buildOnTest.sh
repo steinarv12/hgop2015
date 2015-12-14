a8777359267e52a6d693dbda0485b0856be57e2f
Steinar Valsson <steinarv12@ru.is>
2015-12-14 13:01:19 +0000

    Changed docker run to run git commit version specified

--- buildOnTest.sh
+++ buildOnTest.sh
@@ -7,7 +7,7 @@ ssh $SSHPATH '
 docker stop tic
 docker rm tic
 docker pull steinarv12/tictactoe:$2
-docker run -p 9000:8080 -d --name="tic" -e "NODE_ENV=production" steinarv12/tictactoe
+docker run -p 9000:8080 -d --name="tic" -e "NODE_ENV=production" steinarv12/tictactoe:$2
 	'
 
 rc=$?
