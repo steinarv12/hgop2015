839242dac85784b43dcf8a6f13ad95ef77420b66
Steinar Valsson <steinarv12@ru.is>
2015-12-15 11:57:11 +0000

    Capacity test at capacity

--- server/capacity/tictactoe.load.js
+++ server/capacity/tictactoe.load.js
@@ -3,7 +3,7 @@ var given = require('../fluid-api/tictactoeFluid').given;
 
 it('Should play 1000 games in x seconds.', function (done) {
   var doneCount = 0;
-  var gamesToPlay = 70;
+  var gamesToPlay = 250;
   var x = 7;
 
   this.timeout(x * 1000);
@@ -15,7 +15,18 @@ it('Should play 1000 games in x seconds.', function (done) {
   };
 
     for (var gameId = 0; gameId < gamesToPlay; gameId++) {
-        given(action("Steinar").createGame("TheFirstGame")).withId(gameId)
-        .expect("GameCreated").byUser("Steinar").finish(QED);
+        given(action("Steinar").createGame("GameIdOne").withName("TheFirstGame"))
+            .withId(gameId)
+            .and(action("Gunni").joinGame("GameIdOne"))
+            .and(action("Steinar").placeAt(0,0,"X"))
+            .and(action("Gunni").placeAt(0,1,"O"))
+            .and(action("Steinar").placeAt(0,2,"X"))
+            .and(action("Gunni").placeAt(1,1,"O"))
+            .and(action("Steinar").placeAt(1,0,"X"))
+            .and(action("Gunni").placeAt(2,0,"O"))
+            .and(action("Steinar").placeAt(1,2,"X"))
+            .and(action("Gunni").placeAt(2,2,"O"))
+            .and(action("Steinar").placeAt(2,1,"X"))
+        .expect("GameDraw").byUser("Steinar").finish(QED);
     }
 });
